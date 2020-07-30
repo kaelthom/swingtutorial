b3fbbaa0a9e8e1d05cabc23817f330b2422aeaf2
Mickael Thomere <mickael.thomere@gmail.com>
2020-07-30 14:35:35 +0200

    Swing Tutorial : Buttons with BorderLayout

--- src/main/java/com/openclassrooms/swingtutorial/frames/ButtonFrame.java
+++ src/main/java/com/openclassrooms/swingtutorial/frames/ButtonFrame.java
@@ -1,18 +1,30 @@
 package com.openclassrooms.swingtutorial.frames;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ButtonFrame extends JFrame {
     private JPanel pan = new JPanel();
     private JButton bouton = new JButton("Mon bouton");
 
     public ButtonFrame() {
-        this.setTitle("Bouton");
-        this.setSize(300, 150);
+        this.setTitle("Bouton avec BorderLayout");
+        this.setSize(300, 300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
+        //On définit le layout à utiliser sur le content pane
+        this.setLayout(new BorderLayout());
         //On ajoute le bouton au content pane de la JFrame
-        this.getContentPane().add(bouton);
+        //Au centre
+        this.getContentPane().add(new JButton("CENTER"), BorderLayout.CENTER);
+        //Au nord
+        this.getContentPane().add(new JButton("NORTH"), BorderLayout.NORTH);
+        //Au sud
+        this.getContentPane().add(new JButton("SOUTH"), BorderLayout.SOUTH);
+        //À l'ouest
+        this.getContentPane().add(new JButton("WEST"), BorderLayout.WEST);
+        //À l'est
+        this.getContentPane().add(new JButton("EAST"), BorderLayout.EAST);
         this.setVisible(true);
     }
 }
