3fd345b3c99e91a68b4cd1e12506d10230b11e62
Mickael Thomere <mickael.thomere@gmail.com>
2020-08-04 16:39:28 +0200

    Swing Tutorial : JTextField

--- src/main/java/com/openclassrooms/swingtutorial/MainFrame.java
+++ src/main/java/com/openclassrooms/swingtutorial/MainFrame.java
@@ -1,12 +1,13 @@
 package com.openclassrooms.swingtutorial;
 
-import com.openclassrooms.swingtutorial.frames.RadioButtonFrame;
+import com.openclassrooms.swingtutorial.frames.TextFieldFrame;
 
 public class MainFrame {
 
     public static void main(String[] args) {
 //        MyFrame myFrame = new MyFrame();
-        RadioButtonFrame radioButtonFrame = new RadioButtonFrame();
+//        RadioButtonFrame radioButtonFrame = new RadioButtonFrame();
+        TextFieldFrame textFieldFrame = new TextFieldFrame();
 //        AnimationFrame myFrame = new AnimationFrame();
 //        ButtonFrame buttonFrame = new ButtonFrame();
 //        HorlogeFrame horlogeFrame = new HorlogeFrame();
