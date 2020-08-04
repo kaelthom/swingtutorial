183ddbf1e1d33a67da317ca4ad82b1b8221b1fcd
Mickael Thomere <mickael.thomere@gmail.com>
2020-08-04 16:23:59 +0200

    Swing Tutorial : JRadioButton

--- src/main/java/com/openclassrooms/swingtutorial/MainFrame.java
+++ src/main/java/com/openclassrooms/swingtutorial/MainFrame.java
@@ -1,12 +1,13 @@
 package com.openclassrooms.swingtutorial;
 
-import com.openclassrooms.swingtutorial.frames.AnimationFrame;
+import com.openclassrooms.swingtutorial.frames.RadioButtonFrame;
 
 public class MainFrame {
 
     public static void main(String[] args) {
 //        MyFrame myFrame = new MyFrame();
-        AnimationFrame myFrame = new AnimationFrame();
+        RadioButtonFrame radioButtonFrame = new RadioButtonFrame();
+//        AnimationFrame myFrame = new AnimationFrame();
 //        ButtonFrame buttonFrame = new ButtonFrame();
 //        HorlogeFrame horlogeFrame = new HorlogeFrame();
 //        JCheckBoxFrame jCheckBoxFrame = new JCheckBoxFrame();
