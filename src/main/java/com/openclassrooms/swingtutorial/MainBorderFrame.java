948715f807138eed874d558a91a8796bcdf17d8b
Mickael Thomere <mickael.thomere@gmail.com>
2020-08-06 17:02:24 +0200

    Swing Tutorial : Border

---
+++ src/main/java/com/openclassrooms/swingtutorial/MainBorderFrame.java
@@ -0,0 +1,57 @@
+package com.openclassrooms.swingtutorial;
+
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import java.awt.*;
+
+//CTRL + SHIFT + O pour générer les imports nécessaires
+public class MainBorderFrame extends JFrame {
+
+    private String[] list = {
+            "Bevel Border",
+            "Etched Border",
+            "Line Border",
+            "Matted Border",
+            "Raised Bevel Border",
+            "Title Border",
+            "Compound Border"
+    };
+
+    private Border[] listBorder = {
+            BorderFactory.createBevelBorder(BevelBorder.LOWERED, Color.black, Color.red),
+            BorderFactory.createEtchedBorder(Color.BLUE, Color.GRAY),
+            BorderFactory.createLineBorder(Color.green),
+            BorderFactory.createMatteBorder(5, 2, 5, 2, Color.MAGENTA),
+            BorderFactory.createRaisedBevelBorder(),
+            BorderFactory.createTitledBorder("Titre"),
+            BorderFactory.createCompoundBorder(
+                    BorderFactory.createBevelBorder(BevelBorder.LOWERED, Color.black, Color.blue),
+                    BorderFactory.createMatteBorder(5, 2, 5, 2, Color.MAGENTA)
+            )
+    };
+
+    public MainBorderFrame() {
+        this.setTitle("Les bordures font la fête !");
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(550, 200);
+
+        JPanel pan = new JPanel();
+        for (int i = 0; i < list.length; i++) {
+            JLabel lib = new JLabel(list[i]);
+            lib.setPreferredSize(new Dimension(150, 50));
+            lib.setBorder(listBorder[i]);
+            lib.setAlignmentX(JLabel.CENTER);
+            lib.setHorizontalAlignment(JLabel.CENTER);
+            pan.add(lib);
+        }
+
+        this.getContentPane().add(pan);
+    }
+
+    public static void main(String[] args) {
+        MainBorderFrame demo = new MainBorderFrame();
+        demo.setVisible(true);
+    }
+}
